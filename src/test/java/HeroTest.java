bbf164555a5d621c9324233a70e138baf7f2c5b0
j0hnton <afrikiddo@gmail.com>
2019-04-17 21:12:10 +0300

    Adding tests

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -9,4 +9,62 @@ public class HeroTest {
         assertEquals(true, myHero instanceof Hero);
     }
 
-  }
+    @Test
+    public void Hero_instantiatesCorrectly2_true() {
+        Hero myHero = new Hero("Thor");
+        assertEquals(true, myHero instanceof Hero);
+    }
+
+    @Test
+    public void Hero_instantiatesCorrectly3_true() {
+        Hero myHero = new Hero("Hulk");
+        assertEquals(true, myHero instanceof Hero);
+    }
+
+    @Test
+    public void Hero_instantiatesCorrectly4_true() {
+        Hero myHero = new Hero("Supergirl");
+        assertEquals(true, myHero instanceof Hero);
+    }
+
+    @Test
+    public void Hero_instantiatesCorrectly5_true() {
+        Hero myHero = new Hero("Wonderwoman");
+        assertEquals(true, myHero instanceof Hero);
+
+    }
+
+    @Test
+    public void Hero_instantiatesCorrectly6_true() {
+        Hero myHero = new Hero("Iron man");
+        assertEquals(true, myHero instanceof Hero);
+
+    }
+
+    @Test
+    public void HeroSkill_instantiatesCorrectly_true() {
+        Hero myHero = new Hero("Flying");
+        assertEquals(true, myHero instanceof Hero);
+
+    }
+
+    @Test
+    public void HeroSkill_instantiatesCorrectly2_true() {
+        Hero myHero = new Hero("lightning");
+        assertEquals(true, myHero instanceof Hero);
+
+    }
+
+    @Test
+    public void HeroSkill_instantiatesCorrectly3_true() {
+        Hero myHero = new Hero("Smashing");
+        assertEquals(true, myHero instanceof Hero);
+
+    }
+    @Test
+    public void HeroSkill_instantiatesCorrectly4_true() {
+        Hero myHero = new Hero("Running");
+        assertEquals(true, myHero instanceof Hero);
+
+    }
+}
